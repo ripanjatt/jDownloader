b177897e5f80092adff05d015da71421e136ddfc
Ripan choudhary <bajwaripanpajal@gmail.com>
2021-09-03 18:16:12 +0530

    Added support for android > 9

--- javaDownloader/src/javaDownloader/Downloader.java
+++ javaDownloader/src/javaDownloader/Downloader.java
@@ -1,9 +1,11 @@
 package javaDownloader;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.text.DecimalFormat;
 
@@ -26,7 +28,6 @@ public class Downloader {
 
     public Downloader(String link, String fileName, String downloadPath, long length){
         /*
-        The only Constructor to initialize a Downloader Object!
         params:
         link            ->      the url to the file!
         fileName        ->      the name of the file to be downloaded!
@@ -44,6 +45,20 @@ public class Downloader {
         df = new DecimalFormat("0.00");
     }
 
+    public Downloader(String link, String fileName, long length){
+        /*
+        params:
+        link            ->      the url to the file!
+        fileName        ->      the name of the file to be downloaded!
+        length          ->      the length of the file to be downloaded(can be equals to zero)!
+         */
+        this.progressListener = null;
+        this.link = link;
+        this.fileName = fileName;
+        this.length = length;
+        df = new DecimalFormat("0.00");
+    }
+    
     public void setOnProgressListener(Listeners.ProgressListener progressListener){
         /*
         Method to set ProgressListener!
@@ -60,7 +75,7 @@ public class Downloader {
 
     public void download(){
         /*
-        Main downloading Thread!
+        Downloading Thread for android <= 9!
         Retries every 3000 milliseconds in case of error!
          */
         new Thread(new Runnable() {
@@ -180,6 +195,120 @@ public class Downloader {
         }).start();
     }
 
+    public void download(OutputStream out, InputStream in){
+        /*
+        Downloading Thread for android > 9
+        Retries every 3000 milliseconds in case of error!
+         */
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                FileOutputStream fos = null;
+                if(progressListener != null){
+                    progressListener.onStart();
+                }
+                try {
+                	fos = (FileOutputStream) out;
+                	if(in != null) {
+                		InputStream stream = in;
+                    	ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                        byte[] data = new byte[1024];
+                        int temp;
+                        while((temp = stream.read(data)) != -1) {
+                        	bos.write(data, 0, temp);
+                        }
+                        stream.close();
+                        if(bos.size() > 0) {
+                        	fos.write(bos.toByteArray());
+                        	size += bos.size();
+                        }
+                	}
+                } catch (Exception e){
+                    if(eventListener != null){
+                        eventListener.onError("" + e);
+                    }
+                }
+                while(!isPaused && !isComplete){
+                    /*
+                    Main downloading loop!
+                     */
+                    try{
+                        URL url = new URL(link);
+                        inputStream = url.openStream();
+                        //noinspection ResultOfMethodCallIgnored
+                        inputStream.skip(size);
+                        byte[] data = new byte[1024];
+                        int read;
+                        long time = System.currentTimeMillis();
+                        long temp = 0;
+                        double speed;
+                        while (!isPaused && (read = inputStream.read(data)) != -1) {
+                            /*
+                            This loop reads all the data from InputStream and write a chuck of bytes to the end of the file!
+                             */
+                            assert fos != null;
+                            fos.write(data, 0, read);
+                            size += read;
+                            temp += read;
+                            if(System.currentTimeMillis() - time > 1000){
+                                /*
+                                This block calculates the value of network speed!
+                                It calculates the data downloaded in bytes after every second!
+                                 */
+                                speed = (temp * 1000.0) / ((System.currentTimeMillis() - time) * 1024.0);
+                                time = System.currentTimeMillis();
+                                temp = 0;
+                                double tempSpeed = Double.parseDouble(df.format(speed));
+                                if(progressListener != null){
+                                    progressListener.onSpeedInKB(tempSpeed);
+                                }
+                            }
+                            if(length != 0){
+                                /*
+                                This block calculates the percentage of data downloaded!
+                                 */
+                                double percent = Double.parseDouble(df.format((((size) / (float)length) * 100.0)));
+                                if(progressListener != null){
+                                    progressListener.onProgress(percent, size, length);
+                                }
+                            }
+                        }
+                        if(isPaused) {
+                            if(eventListener != null){
+                                eventListener.onPause();
+                            }
+                            break;
+                        }
+                        isComplete = true;
+                        if(progressListener != null){
+                            progressListener.onComplete();
+                        }
+                    } catch (Exception e){
+                        try{
+                            if(isPaused){
+                                if(eventListener != null){
+                                    eventListener.onPause();
+                                }
+                                break;
+                            }
+                            /*
+                            This block handles any error and retries every 3000 milliseconds if downloading is not paused!
+                             */
+                            if(eventListener != null){
+                                eventListener.isRetrying();
+                            }
+                            Thread.sleep(3000);
+                        } catch (Exception e1){
+                            if (eventListener != null){
+                                eventListener.onError("" + e);
+                            }
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+    
     public void pause(){
         /*
         Method to pause the downloading!
